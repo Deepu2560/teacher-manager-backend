907ef1aa19dc9b095c4413a3724d30bd3a4e828b
unknown <deepanshugulia256@gmail.com>
2022-04-17 21:27:25 +0530

    solving heroku error H10

--- src/Config/db.js
+++ src/Config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 // connect function for building connection with cloud database
 const connect = () => {
   return mongoose.connect(
-    "mongodb+srv://Deepu2560:teacher@cluster0.5jaog.mongodb.net/deepanshu",
+    "mongodb+srv://Deepu2560:teacher@cluster0.5jaog.mongodb.net/deepanshu?retryWrites=true&w=majority",
   );
 };
 
